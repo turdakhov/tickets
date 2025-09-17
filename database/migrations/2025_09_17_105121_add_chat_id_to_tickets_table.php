062899e10c07cc31ddf4bf23053baa178e725d0b
Turdakhov <turdakhov@gmail.com>
2025-09-17 16:00:18 +0500

    Added telegram chat_id to tickets table

---
+++ database/migrations/2025_09_17_105121_add_chat_id_to_tickets_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use DefStudio\Telegraph\Models\TelegraphChat;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->integer('chat_id')->nullable()->after('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->dropColumn('chat_id');
+        });
+    }
+};
