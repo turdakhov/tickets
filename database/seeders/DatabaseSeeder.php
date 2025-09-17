2257e8e0a455c93a8f0de9abdc55f2d453b903e1
Turdakhov <turdakhov@gmail.com>
2025-09-17 19:38:24 +0500

    Disabled Ticket and TicketMessages Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,8 +25,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             CategorySeeder::class,
-            TicketSeeder::class,
-            TicketMessageSeeder::class,
+            // TicketSeeder::class,
+            // TicketMessageSeeder::class,
         ]);
     }
 }
