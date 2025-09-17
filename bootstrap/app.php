5b6c3da3bc813ee12c6af9e8555e7f7c496dd2a9
Turdakhov <turdakhov@gmail.com>
2025-09-17 20:15:51 +0500

    Added Deleting Incomplete Tickets to Cron

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Console\Commands\DeleteIncompleteTicketsCommand;
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -9,8 +11,8 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -24,4 +26,7 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->call(new DeleteIncompleteTicketsCommand)->daily();
     })->create();
